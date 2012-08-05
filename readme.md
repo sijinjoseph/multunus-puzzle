117c242c762593e8458894ca1678dad7460d1403
Sijin Joseph <sijinjoseph@gmail.com>
2012-08-05 10:43:46 -0400

    Added some more info on the pages, add link back to original puzzle site

--- readme.md
+++ readme.md
@@ -2,3 +2,11 @@ Multunus Puzzle
 =================================
 
 Code for [Multunus Software Puzzle (Twitter)](http://twitterpuzzle.herokuapp.com/).
+
+Given a Twitter handle uses the Twitter REST API to get the user status timeline and create a cloud
+of the number of words in the tweets.
+
+Libraries used
+* jQuery tagCloud to create the cloud. 
+* Python and Flask on the server.
+* simplejson for parsing the Twitter REST API results.
--- src/app.py
+++ src/app.py
@@ -11,6 +11,10 @@ import urllib2
 app = Flask(__name__)
 app.jinja_env.trim_blocks = True
 
+MAX_TWEETS = 1000
+INCLUDE_RETWEETS = True
+EXCLUDE_REPLIES = True
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
@@ -21,16 +25,31 @@ def index():
     
 @app.route('/tagcloud/<twitterhandle>', methods=['GET'])
 def tagcloud(twitterhandle):
-    num_words_grouped = read_tweets(twitterhandle)
-    return render_template('tagcloud.html', twitterhandle=twitterhandle, num_words_grouped=num_words_grouped)
+    num_words_grouped = read_tweets(twitterhandle,
+                                    MAX_TWEETS,
+                                    INCLUDE_RETWEETS,
+                                    EXCLUDE_REPLIES)
+    return render_template('tagcloud.html',
+                           twitterhandle=twitterhandle,
+                           number_of_tweets=sum(num_words_grouped.values()),
+                           max_number_of_tweets = MAX_TWEETS,
+                           include_retweets = INCLUDE_RETWEETS,
+                           include_replies = not EXCLUDE_REPLIES,
+                           num_words_grouped=num_words_grouped)
 
-def read_tweets(twitterhandle, num_tweets=200):
-    url = "https://api.twitter.com/1/statuses/user_timeline.json?include_entities=false&include_rts=false&count=200&trim_user=true&screen_name={}".format(twitterhandle)
+def read_tweets(twitterhandle,
+                num_tweets, 
+                include_retweets, 
+                exclude_replies):
+    url = "https://api.twitter.com/1/statuses/user_timeline.json?\
+include_entities=false&trim_user=true&screen_name={}&include_rts={}\
+&exclude_replies={}&count={}".format(twitterhandle, include_retweets,
+                                         exclude_replies, num_tweets)
+    
     f = urllib2.urlopen(url)     
     content = f.read()        
     js_tweets = simplejson.loads(content)
-        
-    app.logger.debug([[js_tweet['text'].split()] for js_tweet in js_tweets])
+    
     num_words = [len(js_tweet['text'].split()) for js_tweet in js_tweets]
     num_words_grouped = {x:num_words.count(x) for x in set(num_words)}
     
